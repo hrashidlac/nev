6cd913cf6517e71ec33d5f66cfb36b719fa9c177
Harris Rashid <hrashid@clippers.com>
2022-09-06 07:24:57 -0700

    adding name and artist from API

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ app.get('/', wrapAsync(async function example1(req, res, next) {
   const { data, error } = await supabasedb.from('votes')
   res.status(200).json({ 
     song1_votes: data[0].song_1,
-    song1_name: 'coming',
-    song1_artist: 'coming',
+    song1_name: data[0].song_1_name,
+    song1_artist: data[0].song_1_artist,
     song2_votes: data[0].song_2,
-    song2_name: 'coming',
-    song2_artist: 'coming',
+    song2_name: data[0].song_2_name,
+    song2_artist: data[0].song_2_artist,
     song3_votes: data[0].song_3,
-    song3_name: 'coming',
-    song3_artist: 'coming',
+    song3_name: data[0].song_3_name,
+    song3_artist: data[0].song_3_artist,
   });
 }));
 
